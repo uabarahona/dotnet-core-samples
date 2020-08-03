5e07d4ebd93f4f0be599727e1dbf53187d10603e
Juan Barahona <barahonajm@outlook.com>
2020-08-03 09:51:22 -0400

    Adding a sample to integrate TailwindCSS with Blazor

---
+++ BlazorWasmTailwindCSS/BlazorWasmTailwindCSS/StaticAssets/webpack.config.js
@@ -0,0 +1,31 @@
+﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+
+module.exports = (env, args) => ({
+    devtool: args.mode === 'development' ? 'source-map' : 'none',
+    entry: './css/app.css',
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, '..', 'wwwroot/css')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new FixStyleOnlyEntriesPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+    ]
+});
